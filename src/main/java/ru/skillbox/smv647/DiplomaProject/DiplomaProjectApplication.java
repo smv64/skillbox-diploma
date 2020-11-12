e1fe23615a0115d9db2a41b45f539913af16d024
smv64 <smv647@gmail.com>
2020-11-12 22:15:42 +0300

    init

---
+++ src/main/java/ru/skillbox/smv647/DiplomaProject/DiplomaProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.skillbox.smv647.DiplomaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skillbox/smv647/DiplomaProject/DiplomaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.smv647.DiplomaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
