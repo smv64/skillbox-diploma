578f24ff5c2ef4a5d7739daaf5a8b74733fa9681
smv64 <smv647@gmail.com>
2020-11-24 15:11:05 +0300

    * convert to flyway

---
+++ src/main/resources/db/migration/V3_0__global_settings.sql
@@ -0,0 +1,4 @@
+INSERT INTO GLOBAL_SETTINGS(code, name, value) VALUES
+('MULTIUSER_MODE','Многопользовательский режим','NO'),
+('POST_PREMODERATION','Премодерация постов','NO'),
+('STATISTICS_IS_PUBLIC','Показывать всем статистику блога','NO')
---
+++ src/main/resources/db/migration/V4_0__test_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USERS(is_moderator, reg_time, name, email, password) VALUES
+(true, current_timestamp, 'TEST_ADMIN1', 'admin1@myblog.ru', 'TEST_ADMIN1'),
+(false, current_timestamp, 'TEST_USER1', 'user1@mail.ru', 'TEST_USER1'),
+(false, current_timestamp, 'TEST_USER2', 'user2@mail.ru', 'TEST_USER2');
+
